4bb1a1011df6306a58f9ca7109dcefe4f5981723
MarkelKhoury <marckhoury566@gmail,com>
2023-01-30 18:48:59 +0200

    Test.java

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test
+{
+	public static void main(String[] args){
+		System.out.println("This is a test");
+		System.out.println("This is 10 devided by 2" + ":" + 10/2);
+		System.out.println("I hope this works.");
+	}
+}
